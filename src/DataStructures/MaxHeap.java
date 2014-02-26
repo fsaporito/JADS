e50e4863ff60d76c3fdf018e7f7fd28a1f6fdda9
Francesco Saporito <sapo93@gmail.com>
2014-02-26 20:10:21 +0100

    Update MaxHeap.java

--- src/DataStructures/MaxHeap.java
+++ src/DataStructures/MaxHeap.java
@@ -2,211 +2,191 @@ package DataStructures;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
+
+import Exceptions.EmptyHeapException;
 
 
 public class MaxHeap <T extends Comparable<T>> {
 
+
 	private ArrayList<T> heapArrList;
 	
-	private T[] heapArr;
-	
-	private ArrayList<T> arrListSorted;
-	
-	private T[] arrSorted;
-	
 	private int heapSize;
-
 	
 	
-	@SuppressWarnings("unchecked")
-	public MaxHeap(ArrayList<T> arrayList) {
+	public MaxHeap () {
 		
-		System.out.println ("Inside");
+		this.heapSize = -1;
 		
-		this.heapSize = arrayList.size();
+		this.heapArrList = new ArrayList<T>();
 		
-		this.heapArrList = arrayList;
-		
-		for (int i =(int) (heapSize/2); i > 0; i--) { 
-			
-			this.MaxHeapify(i);		
-		
-		}
-		
-		this.heapArr = (T[]) this.heapArrList.toArray((T[])Array.newInstance(this.heapArrList.get(0).getClass(), this.heapArrList.size()));
-		
-		this.arrListSorted = new ArrayList<T>();
-		
-		this.HeapSort();
-	
 	}
 	
 	
 	
-	@SuppressWarnings("unchecked")
+	public MaxHeap(ArrayList<T> arrayList) {
+		
+		this.heapSize = arrayList.size() -1;
+		
+		this.heapArrList = arrayList;
+		
+		this.buildHeap();
+		
+	}
+	
+	
+
 	public MaxHeap(T[] array) {
 		
 		this.heapArrList = new ArrayList<T>();
-		
+				
 		for (int i = 0; i < array.length; i++) {
 			
 			this.heapArrList.add(array[i]);
 			
-		}
+		}		
+	
+		this.heapSize = this.heapArrList.size() -1;
 		
-		this.heapSize = this.heapArrList.size();
+		this.buildHeap();
 		
-		System.out.println (heapSize);
+	}
+	
+	
+	
+	private void buildHeap () {
 		
-		for (int i =(int) (heapSize/2); i > 0; i--) { 
+		if (this.heapSize > 0) {		
 			
-			this.MaxHeapify(i);		
-		
+			for (int i =(int) (heapSize/2); i >= 0; i--) { 
+			
+				this.MaxHeapify(i);
+					
+			}
+			
 		}
 		
-		this.heapArr = (T[]) this.heapArrList.toArray((T[])Array.newInstance(this.heapArrList.get(0).getClass(), this.heapArrList.size()));
-		
-		this.arrListSorted = new ArrayList<T>();
-								
-		this.HeapSort();
-		
-	}		
+	}
+	
 	
 	
 	private void MaxHeapify(int i) {
 		
-		System.out.println ("Inside MAxHeapify [i] = " + i);
-		
-		int l = 2*i; // i left son
-		
-		System.out.println ("Left = " + l);
-		
-		int r = 2*i + 1; // i right son
-		
-		System.out.println ("Right = " + r);
-		
-		int max = i;
-		
-		System.out.println ("Max = " + max);
-		
-		if (l <= this.heapSize) {
+		if (this.heapSize > 0) { // More Than One Element
 			
-			if ((this.heapArrList.get(l)).compareTo(this.heapArrList.get(i)) > 0) {
+			int l = 2*i; // i Left Son
+			int r = 2*i + 1; // i Right Son
+		
+			int Max = i;
+		
+			if (l <= this.heapSize) {
 				
-				max = l;
+				if ((this.heapArrList.get(l)).compareTo(this.heapArrList.get(Max)) > 0) {
 				
-				System.out.println ("Max = " + max);
+					Max = l;
 				
+				}
+			
 			}
-			
-		}
 		
-		if (r < this.heapSize) {
+			if (r <= this.heapSize) {
 			
-			if (this.heapArrList.get(r).compareTo(this.heapArrList.get(i)) > 0) {
+				if (this.heapArrList.get(r).compareTo(this.heapArrList.get(Max)) > 0) {
 				
-				max = r;
+					Max = r;
 				
-				System.out.println ("Max = " + max);
-				
+				}
+			
 			}
-			
-		}
 		
-		System.out.println ("Max = " + max);
-		
-		if (max != i) {
+			if (Max != i) {
 			
-			System.out.println ("Max = " + max);
-			
-			T swap = this.heapArrList.get(i); 
-			
-			this.heapArrList.set(i, this.heapArrList.get(max));
-			
-			this.heapArrList.set(max, swap);
-			
-			System.out.println ("Max = " + max);
-			
-			this.MaxHeapify(max);
-			
-		}
+				T swap = this.heapArrList.get(i); 
 				
+				this.heapArrList.set(i, this.heapArrList.get(Max));
+			
+				this.heapArrList.set(Max, swap);
+			
+				this.MaxHeapify(Max);
+			
+			}
+				
+		}
+		
 	}
 	
 	
-	
-	@SuppressWarnings("unchecked")
-	private void HeapSort () {
+	public T extractMaxValue () throws EmptyHeapException {
 		
-		ArrayList<T> ArrTmp = (ArrayList<T>) this.heapArrList;
-		
-		int heapSize_tmp = this.heapSize;
-		
-		for (int i = this.heapArrList.size() - 1; i >= 1; i--) {
+		if (this.heapArrList.size() == 0) {
 			
-			T swap = this.heapArrList.get(i); 
-			
-			this.heapArrList.set(i, this.heapArrList.get(0));
-			
-			this.heapArrList.set(0, swap);
-			
-			this.heapSize--;
-			
-			this.MaxHeapify(0);
+			throw new EmptyHeapException ("Heap Is Empty");
 			
 		}
 		
-		this.heapSize = heapSize_tmp;
+		T MaxValue = this.heapArrList.get(0);
 		
-		this.arrListSorted = this.heapArrList;
+		T lastValue = this.heapArrList.get(this.heapSize);
 		
-		this.heapArrList = ArrTmp;	
+		this.heapArrList.set(0, lastValue);
 		
-		this.arrSorted = (T[]) this.heapArrList.toArray((T[])Array.newInstance(this.heapArrList.get(0).getClass(), this.heapArrList.size()));
+		this.heapArrList.remove(this.heapSize);
+		
+		this.heapSize = this.heapSize - 1;
+		
+		this.MaxHeapify(0);
+		
+		return MaxValue;
+		
+	}
+	
+	
+	public void insert (T value) {
+		
+		this.heapArrList.add(value);
+				
+		this.heapSize++;
+						
+		this.buildHeap();
 		
 		
 	}
 
 
-	public T[] getHeapArr() {
-		
-		return this.heapArr;
-	
-	}
-	
-	
-	public ArrayList<T> getHeapArrList() {
+
+	/**
+	 * @return the heapArrList
+	 */
+	public ArrayList<T> clone() {
 		
 		return this.heapArrList;
 	
 	}
-
 	
-	public ArrayList<T> getArrListSorted() {
 	
-		return this.arrListSorted;
 	
+	/**
+	 * Create An Array With The Elements Of The Max Heap
+	 * @return The Array
+	 */
+	@SuppressWarnings("unchecked")
+	public T[] toArray () {
+		
+		T[] arr = (T[]) this.heapArrList.toArray( (T[])Array.newInstance(this.heapArrList.get(0).getClass(), this.heapSize));
+		
+		return arr;
+		
 	}
 
 
-	public T[] getArrSorted() {
-	
-		return this.arrSorted;
-	
-	}
 
-
-	
-	
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result
-				+ ((arrListSorted == null) ? 0 : arrListSorted.hashCode());
-		result = prime * result + Arrays.hashCode(arrSorted);
-		result = prime * result + Arrays.hashCode(heapArr);
 		result = prime * result
 				+ ((heapArrList == null) ? 0 : heapArrList.hashCode());
 		result = prime * result + heapSize;
@@ -214,8 +194,10 @@ public class MaxHeap <T extends Comparable<T>> {
 	}
 
 
-	
-	
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -224,17 +206,7 @@ public class MaxHeap <T extends Comparable<T>> {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		@SuppressWarnings("rawtypes")
-		MaxHeap other = (MaxHeap) obj;
-		if (arrListSorted == null) {
-			if (other.arrListSorted != null)
-				return false;
-		} else if (!arrListSorted.equals(other.arrListSorted))
-			return false;
-		if (!Arrays.equals(arrSorted, other.arrSorted))
-			return false;
-		if (!Arrays.equals(heapArr, other.heapArr))
-			return false;
+		MaxHeap<?> other = (MaxHeap<?>) obj;
 		if (heapArrList == null) {
 			if (other.heapArrList != null)
 				return false;
@@ -246,15 +218,16 @@ public class MaxHeap <T extends Comparable<T>> {
 	}
 
 
+
+	/** 
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
-		return "MaxHeap [heapArrList=" + heapArrList + ", heapArr="
-				+ Arrays.toString(heapArr) + ", arrListSorted=" + arrListSorted
-				+ ", arrSorted=" + Arrays.toString(arrSorted) + ", heapSize="
-				+ heapSize + "]";
+		return "MaxHeap [heapArrList=" + heapArrList + ", heapSize=" + heapSize
+				+ "]";
 	}
 	
 	
 	
-
 }
